478475580538a197ec81f20a2cb5c78b0bdf6074
Md. Rayhan Shorker <raihanshorker56@gmail.com>
2025-01-17 00:15:02 +0600

    implement action payloads

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,20 @@ function App() {
   // const dispatch = useDispatch();
   // const {count} = useSelector((state)=>state.counter)
   
-  const handleIncrement=()=>{
-    dispatch(increment())
+  const handleIncrement=(amount)=>{
+    dispatch(increment(amount))
   }
-  const handleDecrement=()=>{
-    dispatch(decrement())
+  const handleDecrement=(amount)=>{
+    dispatch(decrement(amount))
   }
   return (
     <div className="flex flex-col h-screen justify-center items-center ">
       <h1 className="text-3xl font-mono  mb-5">Counter With redux</h1>
-      <button onClick={handleIncrement} className="btn btn-success mb-3">Increment</button>
+      <button onClick={()=>{handleIncrement(1)}} className="btn btn-success mb-3">Increment</button>
+      <button onClick={()=>{handleIncrement(5)}} className="btn btn-success mb-3">Increment by 5</button>
       <div>{count}</div>
-      <button onClick={handleDecrement} className="btn mt-3 btn-error">Decrement</button>
+      <button onClick={()=>{handleDecrement(1)}} className="btn mt-3 btn-error">Decrement</button>
+      <button onClick={()=>{ dispatch(decrement(5))}} className="btn mt-3 btn-error">Decrement by 5</button>
     </div>
   );
 }
