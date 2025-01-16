07414dd19c822e2e4ad1513cf36485bae161b3ab
Md. Rayhan Shorker <raihanshorker56@gmail.com>
2025-01-16 23:30:45 +0600

    write actions

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { useDispatch } from "react-redux";
+import { decrement, increment } from "./redux/features/counter/counterSlice";
+
 function App() {
+  const dispatch = useDispatch();
+  const handleIncrement=()=>{
+    dispatch(increment())
+  }
+  const handleDecrement=()=>{
+    dispatch(decrement())
+  }
   return (
     <div className="flex flex-col h-screen justify-center items-center ">
       <h1 className="text-3xl font-mono  mb-5">Counter With redux</h1>
-      <button className="btn btn-success mb-3">Increment</button>
+      <button onClick={handleIncrement} className="btn btn-success mb-3">Increment</button>
       <div>0</div>
-      <button className="btn mt-3 btn-error">Decrement</button>
+      <button onClick={handleDecrement} className="btn mt-3 btn-error">Decrement</button>
     </div>
   );
 }
--- src/redux/features/counter/counterSlice.ts
+++ src/redux/features/counter/counterSlice.ts
@@ -1,11 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
-const initialState={
-    count:0,
+const initialState = {
+  count: 0,
 };
 const counterSLice = createSlice({
-    name:"counter",
-    initialState,
-    reducers:{}
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.count = state.count + 1;
+    },
+    decrement: (state) => {
+      state.count = state.count - 1;
+    },
+  },
 });
-
+export const {increment,decrement} =counterSLice.actions;
 export default counterSLice.reducer;
