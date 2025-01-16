e0b4f6e78ee552b68e2a568e1548d5a4a62d1063
Md. Rayhan Shorker <raihanshorker56@gmail.com>
2025-01-16 23:54:21 +0600

    fix typescript type errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-import { useDispatch } from "react-redux";
+
 import { decrement, increment } from "./redux/features/counter/counterSlice";
+import { useAppDispatch, useAppSelector } from "./redux/hook";
 
 function App() {
-  const dispatch = useDispatch();
+  const dispatch = useAppDispatch();
+  const {count} = useAppSelector((state)=>state.counter)
+  // const dispatch = useDispatch();
+  // const {count} = useSelector((state)=>state.counter)
+  
   const handleIncrement=()=>{
     dispatch(increment())
   }
@@ -13,7 +18,7 @@ function App() {
     <div className="flex flex-col h-screen justify-center items-center ">
       <h1 className="text-3xl font-mono  mb-5">Counter With redux</h1>
       <button onClick={handleIncrement} className="btn btn-success mb-3">Increment</button>
-      <div>0</div>
+      <div>{count}</div>
       <button onClick={handleDecrement} className="btn mt-3 btn-error">Decrement</button>
     </div>
   );
