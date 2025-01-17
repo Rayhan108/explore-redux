c0cd33dd72f486c9d9e209f96e902eb5182af754
Md. Rayhan Shorker <raihanshorker56@gmail.com>
2025-01-17 21:04:38 +0600

    explore function currying

--- src/test.js
+++ src/test.js
@@ -10,3 +10,25 @@ const employee2 = produce(employee,(draft)=>{
 })
 
 console.log(employee,employee2);
+
+//normal function
+
+// const totalPrice = (amount,discount)=>amount - amount*discount;
+
+//function currying
+//ex-1
+function add(a){
+    return function(b){
+        return function(c){
+            return a+b+c;
+        }
+    }
+}
+console.log(add(10)(20)(40));
+// ex-2 :discount
+const totalPrice =(discount)=>(amount)=>amount-amount*discount;
+const withDiscount = totalPrice(0.3);
+
+console.log(withDiscount(100));
+console.log(withDiscount(200));
+console.log(withDiscount(300));
